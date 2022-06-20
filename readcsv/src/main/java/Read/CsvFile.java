165fd43d60c8745009d55097e6af28247378fc5f
dkchoi <comtosa@naver.com>
2022-06-20 16:11:36 +0900

    수정

--- readcsv/src/main/java/Read/CsvFile.java
+++ readcsv/src/main/java/Read/CsvFile.java
@@ -14,37 +14,36 @@ import dto.CsvDto;
 
 public class CsvFile {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 //		readCsvFileScan("D:\\TOP20.csv");
 		readCsvFile("./samples/TOP20.csv");
 	}
 
 	private static void readCsvFile(String filepath) {
 		
-		List<CsvDto> users = new ArrayList<CsvDto>();
+		List<CsvDto> list = new ArrayList<CsvDto>();
 		try {
 			Scanner scanner = new Scanner(new File(filepath));
 			
 			while (scanner.hasNext()) {
 				
-				String[] userData =scanner.nextLine().split(",");
+				String[] Data =scanner.nextLine().split(",");
 				
-				if (!userData[0].equals("id")) {
-					users.add(new CsvDto(userData[0],userData[1],userData[2],userData[3]));
+				if (!Data[0].equals("id")) {
+					list.add(new CsvDto(Data[0],Data[1],Data[2],Data[3]));
 				}
 			}
 			scanner.close();
 			
 			System.out.println("=======================CSV => JSON 저장===========================");
-			System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(users));
+			System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(list));
 			System.out.println("=================================================================");
 			System.out.println("=================================================================");
 			System.out.println("=================================================================");
 			System.out.println("");
 			System.out.println("=======================LicenseOrgan 출력==========================");
-			for(int i=0; i<users.size(); i++)
-			System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(users.get(i).getLicenseOrgan()) );
+			for(int i=0; i<list.size(); i++)
+			System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(list.get(i).getLicenseOrgan()) );
 			System.out.println("================================================================");
 			System.out.println("=================================================================");
 			System.out.println("=================================================================");
